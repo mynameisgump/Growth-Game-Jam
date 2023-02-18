4bf61e82f92ce110bc21cf2cf40ea7d23644df92
TheCrypticCanadian <ecrann@protonmail.com>
2023-02-18 01:23:34 -0330

    Pain and suffering

--- Grumpt.gd
+++ Grumpt.gd
@@ -10,6 +10,7 @@ extends CharacterBody3D
 
 @onready var right_blood_spray : GPUParticles3D = $RightBlood
 @onready var left_blood_spray : GPUParticles3D = $LeftBlood
+@onready var torso_blood_spray : GPUParticles3D = $TorsoBlood
 
 @onready var leftLegGibs : Node3D = $LeftLegGibs
 @onready var rightLegGibs : Node3D = $RightLegGibs
@@ -23,7 +24,6 @@ func damage(hit) -> void:
 		for gib in gibs:
 			gib.visible = true
 		
-		
 	if hit == right_hit.name:
 		right_hit.disabled = true;
 		right_leg.visible = false;
@@ -33,5 +33,14 @@ func damage(hit) -> void:
 			gib.visible = true
 	
 	if hit == body_hit.name:
+		right_hit.disabled = true;
+		right_leg.visible = false;
+		left_hit.disabled = true;
+		left_leg.visible = false;
 		body_hit.disabled = true;
 		body.visible = false;
+		right_blood_spray.emitting = false;
+		left_blood_spray.emitting = false;
+		torso_blood_spray.emitting = true;
+
+
