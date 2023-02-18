7527b79925b290fa941ce5647a36e40328e8bde5
TheCrypticCanadian <ecrann@protonmail.com>
2023-02-18 00:59:30 -0330

    Gibs be pushin

--- Grumpt.gd
+++ Grumpt.gd
@@ -1,27 +1,37 @@
 extends CharacterBody3D
 
-@onready var left_leg : MeshInstance3D = $Left_Leg
-@onready var right_leg : MeshInstance3D = $Right_Leg
-@onready var body : MeshInstance3D = $Body
+@onready var left_leg : MeshInstance3D = $LeftLegHit/LeftLegMesh
+@onready var right_leg : MeshInstance3D = $RightLegHit/RightLegMesh
+@onready var body : MeshInstance3D = $TorsoHit/TorsoMesh
 
-@onready var left_hit : CollisionShape3D = $left_leg_hit
-@onready var right_hit : CollisionShape3D = $right_leg_hit
-@onready var body_hit : CollisionShape3D = $body_hit
+@onready var left_hit : CollisionShape3D = $LeftLegHit
+@onready var right_hit : CollisionShape3D = $RightLegHit
+@onready var body_hit : CollisionShape3D = $TorsoHit
 
-@onready var right_blood_spray : GPUParticles3D = $right_blood_spray
-@onready var left_blood_spray : GPUParticles3D = $left_blood_spray
+@onready var right_blood_spray : GPUParticles3D = $RightBlood
+@onready var left_blood_spray : GPUParticles3D = $LeftBlood
+
+@onready var leftLegGibs : Node3D = $LeftLegGibs
+@onready var rightLegGibs : Node3D = $RightLegGibs
 
 func damage(hit) -> void:
-	if hit == "left_leg_hit":
+	if hit == left_hit.name:
 		left_hit.disabled = true;
 		left_leg.visible = false;
 		left_blood_spray.emitting = true;
+		var gibs = leftLegGibs.get_children()
+		for gib in gibs:
+			gib.visible = true
 		
-	if hit == "right_leg_hit":
+		
+	if hit == right_hit.name:
 		right_hit.disabled = true;
 		right_leg.visible = false;
 		right_blood_spray.emitting = true;
+		var gibs = rightLegGibs.get_children()
+		for gib in gibs:
+			gib.visible = true
 	
-	if hit == "body_hit":
+	if hit == body_hit.name:
 		body_hit.disabled = true;
 		body.visible = false;
